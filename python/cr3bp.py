a0aef8f23a9f1d14b62be3adfe47b71b4802315e
Daniel <danipaezgz@gmail.com>
2021-03-07 19:23:08 +0100

    Added family generation and plotting.

--- python/cr3bp.py
+++ python/cr3bp.py
@@ -120,10 +120,10 @@ class Cr3bp:
 
 class PeriodicOrbit(Cr3bp):
 
-    class OrbitFamily(enum.Enum):
+    class OrbitFamily( enum.Enum ):
         planarLyapunov = 1; verticalLyapunov = 2; northHalo = 3; southHalo = 4
 
-    def runFullSimulationUntilTermination( self, initialState, initialTime, terminationFunction, max_step=1, maxTime=5000.0):      
+    def runFullSimulationUntilTermination( self, initialState, initialTime, terminationFunction, max_step=1, maxTime=5000.0 ):      
         tSpan = [initialTime, maxTime]
         integrator = integrate.solve_ivp(self.modelDynamicsCR3BP, tSpan, initialState, max_step=max_step, events=terminationFunction, dense_output=True) 
         stateTransFun = lambda y, t : self.stateTransitionDerivative(integrator.sol(t), y) 
@@ -131,7 +131,7 @@ class PeriodicOrbit(Cr3bp):
                 
         return integrator, stateTransitionSolution[-1]
 
-    def applyCorrectorPredictor( self, approxInitialState, initialTime, lagrangePoint, orbitFamily, tol=1E-8, returnHalfPeriod=False ):
+    def applyCorrectorPredictor( self, approxInitialState, initialTime, lagrangePoint, orbitFamily, tol=1E-8, returnTerminatingPeriod=False ):
         # Initialize elements
         initialState = approxInitialState
         error = 1
@@ -184,7 +184,7 @@ class PeriodicOrbit(Cr3bp):
                 initialState[0] += xCorrection
                 initialState[4] += yDotCorrection
 
-        if returnHalfPeriod:
+        if returnTerminatingPeriod:
             return initialState, stateEvolution.t[-1]
         else:
             return initialState
@@ -291,7 +291,7 @@ class PeriodicOrbit(Cr3bp):
         except:
             pass
 
-    def continuateSolution(self, initialState, dL):
+    def continuateSolution( self, initialState, dL ):
         u = self.massParameter
         x, y, z, dx, dy, dz = initialState[0], initialState[1], initialState[2], initialState[3], initialState[4], initialState[5]
         r1 = np.sqrt((u + x)**2 + y**2 + z**2)
@@ -307,47 +307,283 @@ class PeriodicOrbit(Cr3bp):
         dH = np.array( [ dHx, dHy, dHz, dHdx, dHdy, dHdz ] )
         return initialState + dL * dH
 
+    def generateDatabase( self, lagrangePoint, orbitFamily, initialParameter=0, initialStateSeed=[], dL=0.1, dLsave=0.1, dLmin=1E-3, totalDeltaL=[], EnergySpan=[], initialTime=0.0, fileName=""):
+        # Function to write in file new data
+        def writeInFile(fileName, energy, state, time):
+            with open(fileName, 'a') as outputFile:
+                outputFile.write('{:.16f}'.format(energy) + " " + '{:.16f}'.format(state[0]) + " " + '{:.16f}'.format(state[1]) + " " + '{:.16f}'.format(state[2]) + \
+                    " " + '{:.16f}'.format(state[3]) + " " + '{:.16f}'.format(state[4]) + " " + '{:.16f}'.format(state[5]) + " " + '{:.16f}'.format(time) + "\n")
 
+        # Create new file
+        if fileName:
+            f = open(fileName, "w")
+            f.close()
+
+        dLorg = dL # Save for later the original dL
+
+        # Check for valid initial condition
+        if not totalDeltaL and not EnergySpan and dLmin == 0:
+            raise ValueError("Invalid stopping conditions.")
+
+        # Compute the seed for the continuation process or use the given initialState
+        if initialStateSeed:
+            initialState = initialStateSeed
+        else:
+            try:
+                print("Computing seed initial state...")
+                approxInitialState = self.getApproximateInitialState(initialParameter, lagrangePoint, orbitFamily)
+                initialState, terminatingPeriod = self.applyCorrectorPredictor(approxInitialState, initialTime, lagrangePoint, orbitFamily, returnTerminatingPeriod=True)
+                print("Completed!")
+            except Exception as error:
+                print(error)
+                raise RuntimeError("There was an error computing the seed state. Try changing the initialParameter value.")
+        
+        dictDatabase = {self.getJacobi(initialState): (initialState, terminatingPeriod)}
+        # Compute continuation, each time dL is too big and the integration throws error dL /= 2 until either dL min is reached or a C boundary.
+        # Start with forward integration
+        keepForwardContinuing = True
+        DeltaLcounter = 0
+        totalDeltaLcounter = 0
+        print("Starting forward continuation...")
+        while keepForwardContinuing:
+            try:
+                continuedState = self.continuateSolution(initialState, dL)
+                initialState, terminatingPeriod = self.applyCorrectorPredictor(continuedState, initialTime, lagrangePoint, orbitFamily, returnTerminatingPeriod=True)
+                DeltaLcounter += dL
+                totalDeltaLcounter += dL
+
+                # Check if the value should be saved and reset counter
+                if DeltaLcounter >= dLsave:
+                    stateEnergy = self.getJacobi(initialState)
+                    dictDatabase.update({stateEnergy: (initialState, terminatingPeriod)})
+                    print("C=",stateEnergy," state saved! Total L=", totalDeltaLcounter)
+                    DeltaLcounter = 0
+
+                    if fileName:
+                        writeInFile(fileName, stateEnergy, initialState, terminatingPeriod)
+
+            except Exception as error:
+                print(error)
+                print("An error with dL = ", dL, "was encountered")
+                dL /= 2.0
+                print("New dL = ", dL)
+
+            # Update the forward loop condition
+            if EnergySpan and dLmin != 0 and totalDeltaLcounter != 0:
+                keepForwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1] and abs(dL) > dLmin and abs(totalDeltaLcounter) < totalDeltaL[0]
+            elif EnergySpan and dLmin != 0 and totalDeltaLcounter == 0:
+                keepForwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1] and abs(dL) > dLmin
+            elif EnergySpan and dLmin == 0 and totalDeltaLcounter != 0:
+                keepForwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1] and abs(totalDeltaLcounter) < totalDeltaL[0]
+            elif EnergySpan and dLmin == 0 and totalDeltaLcounter == 0:
+                keepForwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1]
+            elif not EnergySpan and dLmin != 0 and totalDeltaLcounter != 0:
+                keepForwardContinuing = abs(dL) > dLmin and abs(totalDeltaLcounter) < totalDeltaL[0]
+            elif not EnergySpan and dLmin != 0 and totalDeltaLcounter == 0:
+                keepForwardContinuing = abs(dL) > dLmin
+            elif not EnergySpan and dLmin == 0 and totalDeltaLcounter != 0:
+                keepForwardContinuing = abs(totalDeltaLcounter) < totalDeltaL[0]
+            else:
+                raise ValueError("Error! Invalid stopping conditions")
+        
+        print("Forward continuation completed!")
+        dL = dLorg          # Reset dL
+        keepBackwardContinuing = True
+        DeltaLcounter = 0   # Reset counters
+        totalDeltaLcounter = 0
+        print("Starting backward continuation...")
+        # Backward continuation
+        while keepBackwardContinuing:
+            try:
+                continuedState = self.continuateSolution(initialState, -dL)
+                initialState, terminatingPeriod = self.applyCorrectorPredictor(continuedState, initialTime, lagrangePoint, orbitFamily, returnTerminatingPeriod=True)
+                DeltaLcounter += dL
+                totalDeltaLcounter += dL
+
+                # Check if the value should be saved and reset counter
+                if DeltaLcounter >= dLsave:
+                    stateEnergy = self.getJacobi(initialState)
+                    dictDatabase.update({stateEnergy: (initialState, terminatingPeriod)})
+                    print("C=",stateEnergy," state saved! Total L=", totalDeltaLcounter)
+                    DeltaLcounter = 0
+
+                    # Write in file
+                    if fileName:
+                        writeInFile(fileName, stateEnergy, initialState, terminatingPeriod)
+
+            except Exception as error:
+                print(error)
+                print("An error with dL = ", dL, "was encountered")
+                dL /= 2.0
+                print("New dL = ", dL)
+
+            # Update the forward loop condition
+            if EnergySpan and dLmin != 0 and totalDeltaLcounter != 0:
+                keepBackwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1] and abs(dL) > dLmin and abs(totalDeltaLcounter) < totalDeltaL[1]
+            elif EnergySpan and dLmin != 0 and totalDeltaLcounter == 0:
+                keepBackwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1] and abs(dL) > dLmin
+            elif EnergySpan and dLmin == 0 and totalDeltaLcounter != 0:
+                keepBackwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1] and abs(totalDeltaLcounter) < totalDeltaL[1]
+            elif EnergySpan and dLmin == 0 and totalDeltaLcounter == 0:
+                keepBackwardContinuing = EnergySpan[0] <= self.getJacobi(initialState) <= EnergySpan[1]
+            elif not EnergySpan and dLmin != 0 and totalDeltaLcounter != 0:
+                keepBackwardContinuing = abs(dL) > dLmin and abs(totalDeltaLcounter) < totalDeltaL[1]
+            elif not EnergySpan and dLmin != 0 and totalDeltaLcounter == 0:
+                keepBackwardContinuing = abs(dL) > dLmin
+            elif not EnergySpan and dLmin == 0 and totalDeltaLcounter != 0:
+                keepBackwardContinuing = abs(totalDeltaLcounter) < totalDeltaL[1]
+            else:
+                raise ValueError("Error! Invalid stopping conditions")
+
+        print("Continuation process finished! Returning database dictionary...")
+        return dictDatabase
+
+    def readDatabase( self, fileName="myFile.dat"):
+        myDict = dict()
+        with open(fileName, 'r') as readFile:
+            for line in readFile:
+                energy, x, y, z, dx, dy, dz, t = line.split(" ")
+                myDict.update({float(energy):(np.array([float(x),float(y),float(z),float(dx),float(dy),float(dz)]), float(t[:-2]))})
+
+        return myDict
+
+
+testsOn = False
+computeFamilies = False
 
 oneDay = 86400
 distanceSunEarth = 149597870700 # in m
 var = PeriodicOrbit("Sun","Earth", distanceSunEarth)
- 
 lagrangePointX = var.getLagrangePoint(var.LagrangePoint.l1)[0]
 
-#### Four types of orbit test ####
-lagrangePoint = var.LagrangePoint.l1
-# PLANAR LYAPUNOV
-approxInitialState = var._approxInitialStatePlanar(lagrangePointX-0.0009, lagrangePoint)
-initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.planarLyapunov, returnHalfPeriod=True)
-solData1 = var.runSimulation(initialState, [0.0, 2*halfPeriod], 0.01)
-# HALO NORTH
-approxInitialState = var._approxInitialState3D(1.001, lagrangePoint, var.OrbitFamily.northHalo)
-initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.northHalo, returnHalfPeriod=True)
-solData2 = var.runSimulation(initialState, [0.0, 2*halfPeriod], 0.01)
-# SOUTH NORTH
-approxInitialState = var._approxInitialState3D(1.001, lagrangePoint, var.OrbitFamily.southHalo)
-initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.southHalo, returnHalfPeriod=True)
-solData3 = var.runSimulation(initialState, [0.0, 2*halfPeriod], 0.01)
-# VERTICAL LYAPUNOV
-approxInitialState = var._approxInitialState3D(1, lagrangePoint, var.OrbitFamily.verticalLyapunov)
-initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.verticalLyapunov, returnHalfPeriod=True)
-solData4 = var.runSimulation(initialState, [0.0, 4*halfPeriod], 0.01)
+if testsOn:
+    #### Four types of orbit test ####
+    lagrangePoint = var.LagrangePoint.l1
+    # PLANAR LYAPUNOV
+    approxInitialState = var._approxInitialStatePlanar(lagrangePointX-0.0009, lagrangePoint)
+    initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.planarLyapunov, returnTerminatingPeriod=True)
+    solData1 = var.runSimulation(initialState, [0.0, 2*halfPeriod], 0.01)
+    # HALO NORTH
+    approxInitialState = var._approxInitialState3D(1.001, lagrangePoint, var.OrbitFamily.northHalo)
+    initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.northHalo, returnTerminatingPeriod=True)
+    solData2 = var.runSimulation(initialState, [0.0, 2*halfPeriod], 0.01)
+    # SOUTH NORTH
+    approxInitialState = var._approxInitialState3D(1.001, lagrangePoint, var.OrbitFamily.southHalo)
+    initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.southHalo, returnTerminatingPeriod=True)
+    solData3 = var.runSimulation(initialState, [0.0, 2*halfPeriod], 0.01)
+    # VERTICAL LYAPUNOV
+    approxInitialState = var._approxInitialState3D(1, lagrangePoint, var.OrbitFamily.verticalLyapunov)
+    initialState, halfPeriod = var.applyCorrectorPredictor(approxInitialState, 0.0, lagrangePoint, var.OrbitFamily.verticalLyapunov, returnTerminatingPeriod=True)
+    solData4 = var.runSimulation(initialState, [0.0, 4*halfPeriod], 0.01)
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot3D(solData1.y[0], solData1.y[1], solData1.y[2],'blue')
-ax.plot3D(solData2.y[0], solData2.y[1], solData2.y[2],'black')
-ax.plot3D(solData3.y[0], solData3.y[1], solData3.y[2],'red')
-ax.plot3D(solData4.y[0], solData4.y[1], solData4.y[2],'green')
-ax.scatter(lagrangePointX, 0, 0,  c = 'r', marker='x')
-#ax.scatter(1-var.massParameter, 0, 0,  c = 'r', marker='x')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.show()
+    # Plot solutions
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot3D(solData1.y[0], solData1.y[1], solData1.y[2],'blue')
+    ax.plot3D(solData2.y[0], solData2.y[1], solData2.y[2],'blue')
+    ax.plot3D(solData3.y[0], solData3.y[1], solData3.y[2],'blue')
+    ax.plot3D(solData4.y[0], solData4.y[1], solData4.y[2],'blue')
+    ax.scatter(lagrangePointX, 0, 0,  c = 'r', marker='x')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
 
+elif computeFamilies :
 
+    fileName1 = "planarLyapunov.dat"
+
+    # Continuation tests
+    # lagrangePoint = var.LagrangePoint.l1
+    # var.generateDatabase(lagrangePoint, var.OrbitFamily.planarLyapunov, var.getLagrangePoint(lagrangePoint)[0]-0.0001, 
+    #                     dL=0.001, dLsave=0.01, dLmin=1E-4, totalDeltaL=[2,2], fileName=fileName)
+
+    # Read file
+    myDict1 = var.readDatabase(fileName1)
+
+    # Plot from solution dictionary
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    for key in myDict1:
+        initialState, terminatingPeriod = myDict1[key]
+        solData = var.runSimulation(initialState, [0.0, 4*terminatingPeriod], max_step=0.01)
+        ax.plot3D(solData.y[0], solData.y[1], solData.y[2],'blue')
+    ax.scatter(lagrangePointX, 0, 0,  c = 'r', marker='x')
+    ax.scatter(1-var.massParameter,0,0,'yellow')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+else:
+    # Plot from solution dictionary
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+
+    # PLANAR
+    ax1 = fig.add_subplot(2, 2, 1, projection='3d')
+    fileName = "planarLyapunov.dat"
+    myDict = var.readDatabase(fileName)
+    # Plot data
+    for i, key in enumerate(myDict):
+        initialState, terminatingPeriod = myDict[key]
+        solData = var.runSimulation(initialState, [0.0, 4*terminatingPeriod], max_step=0.01)
+        alpha = i * (1-0.2) / len(myDict)
+        ax1.plot3D(solData.y[0], solData.y[1], solData.y[2],'blue',alpha=alpha)
+
+    # HALO NORTH
+    ax2 = fig.add_subplot(2, 2, 2, projection='3d')
+    fileName = "northHaloL1.dat"
+    myDict = var.readDatabase(fileName)
+    # Plot data
+    for i, key in enumerate(myDict):
+        initialState, terminatingPeriod = myDict[key]
+        solData = var.runSimulation(initialState, [0.0, 4*terminatingPeriod], max_step=0.01)
+        alpha = i * (0.8-0.2) / len(myDict)
+        ax2.plot3D(solData.y[0], solData.y[1], solData.y[2],'red',alpha=alpha)
+
+    # SOUTH NORTH
+    ax3 = fig.add_subplot(2, 2, 3, projection='3d')
+    fileName = "southHaloL1.dat"
+    myDict = var.readDatabase(fileName)
+    # Plot data
+    for i, key in enumerate(myDict):
+        initialState, terminatingPeriod = myDict[key]
+        solData = var.runSimulation(initialState, [0.0, 4*terminatingPeriod], max_step=0.01)
+        alpha = i * (0.8-0.2) / len(myDict)
+        ax3.plot3D(solData.y[0], solData.y[1], solData.y[2],'black',alpha=alpha)
+
+    # VERTICAL
+    ax4 = fig.add_subplot(2, 2, 4, projection='3d')
+    fileName = "verticalLyapunov.dat"
+    myDict = var.readDatabase(fileName)
+    # Plot data
+    for i, key in enumerate(myDict):
+        initialState, terminatingPeriod = myDict[key]
+        solData = var.runSimulation(initialState, [0.0, 4*terminatingPeriod], max_step=0.01)
+        alpha = i * (1-0.2) / len(myDict)
+        ax4.plot3D(solData.y[0], solData.y[1], solData.y[2],'green',alpha=alpha)
+
+    ax1.scatter(lagrangePointX, 0, 0,  c = 'r', marker='x')
+    ax2.scatter(lagrangePointX, 0, 0,  c = 'r', marker='x')
+    ax3.scatter(lagrangePointX, 0, 0,  c = 'r', marker='x')
+    ax4.scatter(lagrangePointX, 0, 0,  c = 'r', marker='x')
+    ax1.set_title('Planar Lyapunov')
+    ax2.set_title('North Halo')
+    ax3.set_title('South Halo')
+    ax4.set_title('Vertical Lyapunov')
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+    ax2.set_xlabel('X')
+    ax2.set_ylabel('Y')
+    ax2.set_zlabel('Z')
+    ax3.set_xlabel('X')
+    ax3.set_ylabel('Y')
+    ax3.set_zlabel('Z')
+    ax4.set_xlabel('X')
+    ax4.set_ylabel('Y')
+    ax4.set_zlabel('Z')
+    plt.show()
 
 
 
